c61aa61cdd953e67b48f971e49c7a17bc09fffea
Quentin Ma <quentin.ma@etu.u-paris.fr>
2021-02-22 22:02:09 +0100

    improve time complexity for k-core

--- src/main/java/tp3/Coreness.java
+++ src/main/java/tp3/Coreness.java
@@ -17,38 +17,39 @@ public class Coreness {
     }
 
     void init(Graph g) {
-        for (int i = 0; i < g.n; i++) {
+        for (int i = 0; i < g.n; i++)
             queue.offer(new AbstractMap.SimpleEntry<>(i, g.degree(i)));
-        }
     }
 
     void undo(Graph g, int u) {
         isActivate[u] = false;
-        n--;
+        --n;
         g.deg[u] = 0;
-        for (int v : g.neighbors(u)) {
-            g.deg[v]--;
+        queue.poll();
+        if (n == 0) {
+            return;
         }
-    }
-
-    void updateQueue(Graph g) {
-        queue.clear();
-        for (int i = 0; i < g.deg.length; ++i) {
-            if (isActivate[i])
-                queue.offer(new AbstractMap.SimpleEntry<>(i, g.deg[i]));
+        for (int v : g.neighbors(u)) {
+            if (isActivate[v]) {
+                g.deg[v]--;
+                queue.offer(new AbstractMap.SimpleEntry<>(v, g.deg[v]));
+            }
         }
     }
 
     void coreness(Graph g) {
-        for (int k = 0; k < g.n; ++k) {
+        for (int k = 0; k < g.n + 1; ++k) {
             int last = n;
             while (true) {
                 assert queue.peek() != null;
                 if (!(queue.peek().getValue() < k)) break;
-                Integer node = queue.peek().getKey();
-                undo(g, node);
-                updateQueue(g);
-                if (queue.size() == 0) {
+                if (g.deg[queue.peek().getKey()] == queue.peek().getValue()) {
+                    Integer node = queue.peek().getKey();
+                    undo(g, node);
+                } else {
+                    queue.poll();
+                }
+                if (n == 0) {
                     System.out.format("%d\n", k - 1);
                     System.out.format("%d\n", last);
                     return;
